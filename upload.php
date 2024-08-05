812bd8287ea04f1dce40af3a288e0feeba216059
arthurdias03 <ardiangelino@gmail.com>
2024-08-05 16:47:26 -0300

    novo campo no banco de dados e mudancas nas paginas do site

--- upload.php
+++ upload.php
@@ -11,6 +11,7 @@ include 'config.php';
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,60 +19,63 @@ include 'config.php';
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-body-blue" aria-label="Tenth navbar example">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class=" nav-link2" aria-current="page" href="Home.php" >Home</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2 " href="Sobre.php">Sobre</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" href="Equipe.php">Equipe </a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" aria-disabled="true" href="publicacoesteste.php">Publicações</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" aria-disabled="true" href="Aplicativos.php">Aplicativos</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" aria-disabled="true" href="Materiais.php">Materiais</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" aria-disabled="true" href="Contato.php">Contato</a>
-            </li>
-            <li class="nav-item">
-              <a class=" nav-link2" aria-disabled="true" href="login.php">Admin</a>
-            </li>
-          </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-body-blue" aria-label="Tenth navbar example">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08"
+                aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-current="page" href="Home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2 " href="Sobre.php">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" href="Equipe.php">Equipe </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-disabled="true" href="publicacoesteste.php">Publicações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-disabled="true" href="Aplicativos.php">Aplicativos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-disabled="true" href="Materiais.php">Materiais</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-disabled="true" href="Contato.php">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" nav-link2" aria-disabled="true" href="login.php">Admin</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </nav>
 
     <!--logo site-->
     <div class="icon-menu">
-      <div class="row align-items-center">
-        <div class="col-auto">
-          <div class="logo">
-            <img src="Img/icones/Logo_rumoPNG.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
-          </div> 
+        <div class="row align-items-center">
+            <div class="col-auto">
+                <div class="logo">
+                    <img src="Img/icones/Logo_rumoPNG.png" alt="Logo" width="100" height="100"
+                        class="d-inline-block align-text-top">
+                </div>
+            </div>
+            <div class="col">
+                <h2>Rumo á Educação Matemática Inclusiva</h2>
+            </div>
         </div>
-        <div class="col">
-          <h2>Rumo á Educação Matemática Inclusiva</h2>
-        </div>
-      </div>
     </div>
 
 
-    <div class="container mt-5">
+    <div class="container mt-5" style="margin-bottom:20%">
     <h2>Upload de Arquivo PDF</h2>
     <a href="logout.php" class="btn btn-danger mb-3">Logout</a>
     <?php
@@ -80,6 +84,7 @@ include 'config.php';
         $categoria = $_POST['categoria'];
         $ano = $_POST['ano'];
         $autor = isset($_POST['novo_autor']) && !empty($_POST['novo_autor']) ? $_POST['novo_autor'] : $_POST['autor'];
+        $periodo = $_POST['periodo'];
         $arquivo = $_FILES['arquivo'];
 
         if ($arquivo['type'] == 'application/pdf' && $arquivo['size'] > 0) {
@@ -95,7 +100,8 @@ include 'config.php';
             }
 
             if (move_uploaded_file($arquivo['tmp_name'], $caminhoArquivo)) {
-                $sql = "INSERT INTO arquivos (nome_arquivo, descricao, caminho_arquivo, categoria, ano, autor) VALUES ('$nomeArquivo', '$descricao', '$caminhoArquivo', '$categoria', '$ano', '$autor')";
+                $sql = "INSERT INTO arquivos (nome_arquivo, descricao, caminho_arquivo, categoria, ano, autor, periodo) 
+                        VALUES ('$nomeArquivo', '$descricao', '$caminhoArquivo', '$categoria', '$ano', '$autor', '$periodo')";
                 if ($conn->query($sql) === TRUE) {
                     echo '<div class="alert alert-success">Arquivo enviado com sucesso!</div>';
                 } else {
@@ -151,33 +157,46 @@ include 'config.php';
             <input class="form-control mt-2" type="text" id="novo_autor" name="novo_autor" placeholder="Novo Autor (se aplicável)">
         </div>
         <div class="mb-3">
+            <label for="periodo" class="form-label">Período</label>
+            <select class="form-control" id="periodo" name="periodo" required>
+                <option value="periodico">Periódico</option>
+                <option value="anuais">Anuais</option>
+            </select>
+        </div>
+        <div class="mb-3">
             <label for="arquivo" class="form-label">Arquivo PDF</label>
             <input class="form-control" type="file" id="arquivo" name="arquivo" accept="application/pdf" required>
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
-    </div>
+</div>
 
     <footer class="navbar fixed-bottom bg-footer">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="Home.php"><img src="Img/icones/Logo_rumoPNG.png" alt="Logo" width="70" height="50" class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
-        <div class="d-flex">
-          <a class="navbar-brand"><img src="Img/icones/logo_fapesp.png" alt="Logo" width="90" height="50" class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
-          <a class="navbar-brand"><img src="Img/icones/CNPq_v2017_rgb.png" alt="Logo" width="90" height="40" class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
-          <a class="navbar-brand"><img src="Img/icones/banner_capes-1024x871.png" alt="Logo" width="70" height="50" class="d-inline-block align-text-top" style="margin-left: 15px; object-fit: contain;"></a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Home.php"><img src="Img/icones/Logo_rumoPNG.png" alt="Logo" width="70"
+                    height="50" class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
+            <div class="d-flex">
+                <a class="navbar-brand"><img src="Img/icones/logo_fapesp.png" alt="Logo" width="90" height="50"
+                        class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
+                <a class="navbar-brand"><img src="Img/icones/CNPq_v2017_rgb.png" alt="Logo" width="90" height="40"
+                        class="d-inline-block align-text-top" style="margin-left: 15px;"></a>
+                <a class="navbar-brand"><img src="Img/icones/banner_capes-1024x871.png" alt="Logo" width="70"
+                        height="50" class="d-inline-block align-text-top"
+                        style="margin-left: 15px; object-fit: contain;"></a>
+            </div>
         </div>
-      </div>
     </footer>
 
 
-<script>
-document.getElementById('novo_autor').addEventListener('input', function() {
-    if (this.value) {
-        document.getElementById('autor').disabled = true;
-    } else {
-        document.getElementById('autor').disabled = false;
-    }
-});
-</script>
+    <script>
+    document.getElementById('novo_autor').addEventListener('input', function() {
+        if (this.value) {
+            document.getElementById('autor').disabled = true;
+        } else {
+            document.getElementById('autor').disabled = false;
+        }
+    });
+    </script>
 </body>
+
 </html>
